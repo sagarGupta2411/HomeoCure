03365185e9bd76ca140be45be75bf2dac179515b
swaraj <swaraj12024@gmail.com>
2018-06-30 21:27:45 +0530

    initial

--- app/src/main/java/in/aplicatia/homeocure/SplashScreen.java
+++ app/src/main/java/in/aplicatia/homeocure/SplashScreen.java
@@ -1,13 +1,54 @@
 package in.aplicatia.homeocure;
 
+import android.animation.Animator;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ImageView myView = findViewById(R.id.main);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    // get the center for the clipping circle
+                    int cx = myView.getWidth() / 2;
+                    int cy = myView.getHeight() / 2;
+
+                    // get the final radius for the clipping circle
+                    float finalRadius = (float) Math.hypot(cx, cy);
+
+                    // create the animator for this view (the start radius is zero)
+                    Animator anim =
+                            ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
+
+                    // make the view visible and start the animation
+                    myView.setBackgroundColor(Color.BLUE);
+                    anim.setDuration(2000);
+                    anim.start();
+                } else {
+                    // set the view to visible without a circular reveal animation below Lollipop
+                    myView.setVisibility(View.VISIBLE);
+                }
+            }
+        },3000);
+// Check if the runtime version is at least Lollipop
+
     }
 }
