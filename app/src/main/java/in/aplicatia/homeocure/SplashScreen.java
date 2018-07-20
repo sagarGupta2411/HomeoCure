38d1dcf3998ad65937d71636f78550c62d257bc3
swaraj12024 <swaraj12024@gmail.com>
2018-07-20 17:25:55 +0530

    initial commit

--- app/src/main/java/in/aplicatia/homeocure/SplashScreen.java
+++ app/src/main/java/in/aplicatia/homeocure/SplashScreen.java
@@ -2,11 +2,13 @@ package in.aplicatia.homeocure;
 
 import android.animation.Animator;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.widget.ImageView;
@@ -20,6 +22,14 @@ public class SplashScreen extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, LoginActivity.class));
+            }
+        }, 5000);
 
 
         new Handler().postDelayed(new Runnable() {
@@ -40,20 +50,7 @@ public class SplashScreen extends Activity {
                             ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, finalRadius);
 
                     // make the view visible and start the animation
-                    myView.setBackgroundColor(getResources().getColor(R.color.colorPrimary
-
-
-
-
-
-
-
-
-
-
-
-
-                    ));
+                    myView.setBackground(getDrawable(R.drawable.gradient));
                     anim.setDuration(2000);
                     anim.start();
                 } else {
